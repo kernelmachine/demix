ef6a982613ac488bf8cad20f3c5ba3951ec615f2
Suchin Sri Gururangan <suching@fb.com>
2021-08-09 14:36:16 -0700

    push

--- fairseq_cli/ensemble_eval_lm.py
+++ fairseq_cli/ensemble_eval_lm.py
@@ -279,7 +279,7 @@ def dynamic_eval_lm(
             if not precomputed_prior:
                 prior = pd.DataFrame(np.concatenate(expert_probs_all,0)).ewm(alpha=0.3, adjust=False).mean().tail(n=1).to_numpy().squeeze(0)
             priors_all.append(prior)
-        dev_batch_iterator.tqdm.set_description(target_ppl.item())
+        dev_batch_iterator.tqdm.set_description(f"ppl: {target_ppl.item()}")
     return target_ppls, expert_probs_all, priors_all
 
 
